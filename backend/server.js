6585cd2363627e09137ee1e0ac3d748cdd45d6df
BookHub <abhishek160704kumar@gmail.com>
2025-04-22 17:39:39 +0530

    added res from backend to the application state

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ connectDb();
 //rest object
 const app = express();
 
+
 //middlewares
 app.use(morgan('dev'));
 app.use(express.json());
@@ -21,9 +22,9 @@ app.use(cors());
 
 
 //routes
-// app.get('/', (req,res) =>{
-//     res.send("<h1>Hello From Server</h1>");
-// })
+app.get('/', (req,res) =>{
+    res.send("Hello From Abhishek server");
+})
 
 //user routes
 
